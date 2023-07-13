83464e1e288f44e33d9191812daf3bb0e12553eb
ErickMarchetti <erickgmarchetti@gmail.com>
2023-07-13 14:45:59 -0300

    feat: padronização de erros

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express"
+import AppError from "../errors/appError"
+
+const errorMiddleware = (
+  err: Error,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: "error",
+      code: err.statusCode,
+      message: err.message
+    })
+  }
+
+  console.error(err)
+
+  return response.status(500).json({
+    status: "error",
+    code: 500,
+    message: "Internal server error"
+  })
+}
+
+export default errorMiddleware
