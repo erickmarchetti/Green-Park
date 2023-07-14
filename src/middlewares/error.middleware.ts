05ddaab40b5cd5c12c389a71712db276b9d2b0be
ErickMarchetti <erickgmarchetti@gmail.com>
2023-07-14 12:34:14 -0300

    feat: tratativa de erros do multer

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,23 +1,32 @@
 import { Request, Response, NextFunction } from "express"
 import AppError from "../errors/appError"
+import { MulterError } from "multer"
 
 const errorMiddleware = (
   err: Error,
-  request: Request,
-  response: Response,
+  req: Request,
+  res: Response,
   next: NextFunction
 ) => {
   if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
+    return res.status(err.statusCode).json({
       status: "error",
       code: err.statusCode,
       message: err.message
     })
   }
 
+  if (err instanceof MulterError) {
+    return res.status(400).json({
+      status: "error",
+      code: 400,
+      message: `Error in the ${err.field} field. ${err.message}`
+    })
+  }
+
   console.error(err)
 
-  return response.status(500).json({
+  return res.status(500).json({
     status: "error",
     code: 500,
     message: "Internal server error"
